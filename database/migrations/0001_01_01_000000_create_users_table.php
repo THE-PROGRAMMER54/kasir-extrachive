401a866dd7576b34d550c07222d5e2dc56b491d1
Reihan Saputra <sreihan513@gmail.com>
2025-03-16 06:20:07 +0700

    database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('gambar')->default('profile.jpg');
+            $table->enum('role',['admin','kasir'])->default('kasir');
             $table->string('password');
             $table->timestamps();
         });
